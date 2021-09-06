800beb7cdc9b36d3661c3d43274946ec00cc46f0
Shaum Chen <ShaumChen@outlook.com>
2021-09-06 11:09:06 +0800

    go mod

--- controller/Index.go
+++ controller/Index.go
@@ -3,10 +3,9 @@ package controller
 import (
 	"scgin/context"
 	"scgin/response"
-
 )
 
 func Index(context *context.Context) *response.Response {
-	panic("something error")
+	//panic("something error")
 	return response.Resp().String(context.Context.FullPath())
 }
